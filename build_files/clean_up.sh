7e10aee5d2b86f22ba1fc84561d0ff4b4bb628bd
Uzodimma <Charltonujic@rocketmail.com>
2020-09-14 15:27:34 +0000

    Create lint.sh file

--- build_files/clean_up.sh
+++ build_files/clean_up.sh
@@ -1,3 +1,6 @@
 # Remove all dangling Docker files
 echo 'Cleaning up...'
 docker system prune -f
+
+# Remove created temporary files
+rm -r /tmp/jenkins_build_files/
---
+++ build_files/lint.sh
@@ -0,0 +1,5 @@
+# lint dockerfile
+hadolint Dockerfile
+
+# lint HTML file
+tidy -q -e *.html
