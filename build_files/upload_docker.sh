3949ff9bd93a923c249eb899550f7bcaa7a46a20
Uzodimma <Charltonujic@rocketmail.com>
2020-09-14 16:37:41 +0000

    Add image tag to docker upload path name

--- build_files/upload_docker.sh
+++ build_files/upload_docker.sh
@@ -1,5 +1,5 @@
 # Tag docker image and upload to DockerHub
-dockerpath=$(<./variables/dockerhub_image_path.txt)/$(<./variables/docker_image_name.txt)
+dockerpath=$(<./variables/dockerhub_image_path.txt)/$(<./variables/docker_image_name.txt):$(<./variables/docker_image_tag.txt)
 local_tag=$(<./variables/docker_image_name.txt):$(<./variables/docker_image_tag.txt)
 docker tag $local_tag $dockerpath
 
