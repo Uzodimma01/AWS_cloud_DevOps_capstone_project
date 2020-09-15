4a5216fb57612be98f1d005776c982ef749efc4f
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-15 02:48:15 -0700

    Update path to deployment file in 'copy_deployment.sh' script

--- build_files/copy_deployment_file.sh
+++ build_files/copy_deployment_file.sh
@@ -1,2 +1,2 @@
 # Copy deployment file to a temporary locationin order to replace variables out of place using sed command
-cp deployment.yml /tmp/jenkins_build_files/
+cp ./build_files/deployment.yml /tmp/jenkins_build_files/
