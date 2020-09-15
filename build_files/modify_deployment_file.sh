e1dc8ec5f0b6c6aef623ae39daf30efb973ab505
Uzodimma01 <50324396+Uzodimma01@users.noreply.github.com>
2020-09-15 05:31:38 -0700

    Correct wrong path to variable files

--- build_files/modify_deployment_file.sh
+++ build_files/modify_deployment_file.sh
@@ -1,9 +1,9 @@
 # Create variables for deployment file
-docker_image_name=$(<../variables/docker_image_name.txt):$(<../variables/docker_image_tag.txt)
+docker_image_name=$(<../variables/docker_image_name.txt)
 replica_set=$(<../variables/node_replica_set.txt)
 max_surge=$(<../variables/max_surge.txt)
 max_unavailable=$(<../variables/max_unavailable.txt)
-docker_image_path=$(<./variables/dockerhub_image_path.txt)/$(<./variables/docker_image_name.txt):$(<./variables/docker_image_tag.txt)
+docker_image_path=$(<./variables/dockerhub_image_path.txt)/$(<./variables/docker_image_name.txt)
 
 # Update variable values in the deployment.yml file
 sed -i "s+docker_image_name+$docker_image_name+g" /tmp/jenkins_build_files/deployment.yml
